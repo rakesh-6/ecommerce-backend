d6d7173c104cc867f1ee15aacd3fddc33c5265cf
Rakesh MM <rakesh060707@gmail.com>
2026-02-21 21:46:03 +0530

    Fixed duplicate errorHandler declaration

--- server.js
+++ server.js
@@ -1,45 +1,37 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
 const connectDB = require("./config/db");
-connectDB();
+const cors = require("cors");
+
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 const orderRoutes = require("./routes/orderRoutes");
-const errorHandler = require("./middleware/errorMiddleware");
-const cors = require("cors");
-const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use("/api/products", productRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/orders", orderRoutes);
-app.use(notFound);
-app.use(errorHandler);
 app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true,
 }));
 
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log("Mongo Error:", err));
-
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.get("/", (req, res) => {
   res.send("API Running...");
 });
 
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log(`Server running on port ${PORT}`);
 });
-
-console.log("User Routes Loaded");
