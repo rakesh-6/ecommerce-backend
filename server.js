bdafb153f4d0d96d166e92885fc1f9a54956b3a0
Rakesh MM <rakesh060707@gmail.com>
2026-02-21 22:47:15 +0530

    Fix CORS

--- server.js
+++ server.js
@@ -16,10 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-}));
+app.use(cors());
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
